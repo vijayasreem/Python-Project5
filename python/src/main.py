4fa866bb1dbcb23f9a5f59ff1df8c5f857477515
Kernel-rb <matabsaifeddine@gmail.com>
2023-12-17 22:00:30 +0100

    Add logging and improve user input validation

--- python/src/main.py
+++ python/src/main.py
@@ -1,84 +1,130 @@
-# i'm creating a banking management system
+import os
 import datetime
 import random
 import uuid
 import sqlite3
-i
+import logging
+
+log_dir = "C:\\Users\\petmk\\Desktop\\BankingManagement\\python\\log"
+log_file = os.path.join(log_dir, "main.log")
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s %(message)s')
+log_formatter = logging.Formatter('%(asctime)s %(levelname)s %(name)s %(message)s\n' + ('=' * 50) + '\n')
+logging.info("Logging is working")
+logging.info("Program Started")
+
+
 
 def name():
-    input_name = input("Enter your name: ")
-    if input_name.isalpha():
-        return input_name
-    else:
-        print("Invalid Name, Only Alphabets Allowed")
-        name()
+    while True:
+        input_name = input("Enter your name: ")
+        if input_name.isalpha():
+            logging.info(f"User name is {input_name}")
+            return input_name
+        else:
+            print("Invalid Name, Only Alphabets Allowed")
+            logging.error("Invalid Name, Only Alphabets Allowed")
+
 
 def age():
     input_age = int(input("Enter your age: "))
     if input_age < 18:
         print("You are not eligible to create an account")
+        logging.error("You are not eligible to create an account")
         exit()
-    elif input_age > 18:
+    elif input_age >= 18:
+        logging.info(f"User is {input_age} years old")
         return input_age
-    
 
-def adress():
-    # TODO : check if adress is valid
-    input_adress = input("Enter your adress: ")
-    return input_adress
+
+def address():
+    input_address = input("Enter your address: ")
+    logging.info(f"User address is {input_address}")
+    return input_address
+
 
 def phone_number():
-    prefix = input("Enter the prefix for your phone number : ")
-    if len(str(prefix)) == 3:
-        pass
-    else:
-        print("Invalid Prefix, Only 3 Digits Allowed")
-        phone_number()
-    input_phone_number = int(input("Enter the remaining digits of your phone number: "))
-    if len(str(input_phone_number)) == 7:
-        return prefix + str(input_phone_number)
-    else:
-        print("Invalid Phone Number, Only 7 Digits Allowed")
-        phone_number()
+    while True:
+        prefix = input("Enter the prefix for your phone number: ")
+        if len(str(prefix)) == 3:
+            logging.info(f"User phone number prefix is {prefix}")
+        else:
+            print("Invalid Prefix, Only 3 Digits Allowed")
+            logging.error("Invalid Prefix, Only 3 Digits Allowed")
+            continue
+
+        input_phone_number = int(input("Enter the remaining digits of your phone number: "))
+
+        if len(str(input_phone_number)) == 7:
+            logging.info(f"User phone number is {prefix + str(input_phone_number)}")
+            return prefix + str(input_phone_number)
+        else:
+            print("Invalid Phone Number, Only 7 Digits Allowed")
+            logging.error("Invalid Phone Number, Only 7 Digits Allowed")
 
 
 def id_generator():
     unique_id = str(uuid.uuid4())
+    logging.info(f"User ID is {unique_id}")
     return unique_id
 
+
 def balance():
-    if balance == None:
-        return 0
-    else:
-        return f"Your balance is {balance}$"
+    logging.info(f"User Balance is {0}")
+    return 0
+
 
 def welcome(user_name, user_id, user_balance):
-    return f"""
+    welcome_message = f"""
     Welcome {user_name} to Kernel Bank 🏦  :
     Your ID is {user_id}
     Your Balance is {user_balance}
     Your Account was created on {datetime.datetime.now()}
-    if you want to edit your account details
-    if you have any questions, please contact us at Kernel-rb on github
+    If you want to edit your account details
+    If you have any questions, please contact us at Kernel-rb on GitHub
     """
+    logging.info("All User Details")
+    logging.info(f"User Welcome Message: {welcome_message}")
+    return welcome_message
+
 
 def create_account():
-    print("Creating Account")
+    print("Creating Account...")
     user_name = name()
-    user_age  = age()
-    user_adress  = adress()
+    user_age = age()
+    user_address = address()
     user_phone_number = phone_number()
     user_id = id_generator()
     user_balance = balance()
     user_welcome_message = welcome(user_name, user_id, user_balance)
+    print(user_welcome_message)
+    logging.info("User Account Created Successfully")
+
+
+def edit_account_details():
+    pass
+
+def deposit_money():
+    pass
+def withdraw_money():
+    pass
+def check_balance():
+    pass
+def display_account_details():
+    pass
+def transfer_money():
+    pass
+def delete_account():
+    pass
 
 
 def main():
+    logging.info("Program Started")
     print(
         """
     Welcome to Kernel Bank 🏦  : 
     1. Create Account
-    2- Edit Account Details
+    2. Edit Account Details
     3. Deposit Money
     4. Withdraw Money
     5. Check Balance
@@ -89,24 +135,40 @@ def main():
         """
     )
     choice = input("Enter your choice: ")
-    if type(choice) == int:
+    logging.info(f"User Choice is {choice}")
+    if choice.isdigit():
+        choice = int(choice)
         if choice == 1:
             create_account()
+            logging.info("User Account Created Successfully")
         elif choice == 2:
             edit_account_details()
+            logging.info("User Account Details Edited Successfully")
         elif choice == 3:
             deposit_money()
+            logging.info("User Money Deposited Successfully")
         elif choice == 4:
             withdraw_money()
+            logging.info("User Money Withdrawn Successfully")
         elif choice == 5:
             check_balance()
+            logging.info("User Balance Checked Successfully")
         elif choice == 6:
             display_account_details()
+            logging.info("User Account Details Displayed Successfully")
         elif choice == 7:
             transfer_money()
+            logging.info("User Money Transferred Successfully")
         elif choice == 8:
             delete_account()
+            logging.info("User Account Deleted Successfully")
         elif choice == 9:
             exit()
+            logging.info("Program Exited Successfully")
     else:
         print("Invalid Choice , Only Numbers Allowed")
+        logging.error("Invalid Choice , Only Numbers Allowed")
+
+
+if __name__ == "__main__":
+    main()
